0a65d96e5251923a50f7e5fe3271c0da142c2ef2
Panos <panoslin@foxmail.com>
2024-06-26 15:08:31 +0800

    fix: routing issue after migration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import {enableProdMode, importProvidersFrom} from '@angular/core';
-
-
-import {environment} from './environments/environment';
 import {bootstrapApplication} from '@angular/platform-browser';
+import {provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app/app.component';
-import {AppRoutingModule} from './app/app-routing.module';
+import {routes} from './app/app.routes';
 import {ProductService} from './app/demo/service/product.service';
 import {PhotoService} from './app/demo/service/photo.service';
 import {NodeService} from './app/demo/service/node.service';
@@ -13,6 +12,8 @@ import {EventService} from './app/demo/service/event.service';
 import {CustomerService} from './app/demo/service/customer.service';
 import {CountryService} from './app/demo/service/country.service';
 import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {environment} from './environments/environment';
 
 if (environment.production) {
     enableProdMode();
@@ -20,10 +21,20 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(AppRoutingModule),
+        provideRouter(
+            routes,
+            withEnabledBlockingInitialNavigation(),
+            withInMemoryScrolling({
+                scrollPositionRestoration: 'enabled',
+                anchorScrolling: 'enabled',
+                // onSameUrlNavigation: 'reload'
+            })
+        ),
         {provide: LocationStrategy, useClass: HashLocationStrategy},
         CountryService, CustomerService, EventService, IconService, NodeService,
-        PhotoService, ProductService
+        PhotoService, ProductService,
+        importProvidersFrom(BrowserAnimationsModule), // Add BrowserAnimationsModule if you use animations
+        importProvidersFrom(HttpClientModule) // Add HttpClientModule here
     ]
 })
     .catch(err => console.error(err));
